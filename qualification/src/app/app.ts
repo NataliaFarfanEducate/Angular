c079d6500231ea38fdb3adfd6375dbfb3aa0c0aa
NataliaFarfanEducate <mfarfan@uniempresarial.edu.co>
2025-09-12 10:18:29 -0500

    ju

--- qualification/src/app/app.ts
+++ qualification/src/app/app.ts
@@ -14,4 +14,22 @@ import { DirectiveNgFor } from './directive-ng-for/directive-ng-for';
 export class App {
   materiaSeleccionada:string ='Matematicas';
   qualification: number = 0;
+seccion= [
+    "En Angular, las directivas son clases que permiten agregar comportamientos adicionales a elementos del DOM o incluso transformar estos elementos y su contenido. Piensa en las directivas como instrucciones que puedes aplicar a los elementos de tu página para manipular sus propiedades, estructura, o incluso su comportamiento en tiempo de ejecución.",
+    { titulo: 'NgIf', descripcion: 'Texto de la sección 1' },
+    { titulo: 'NgFor', descripcion: 'Texto de la sección 2' },
+    { titulo: 'NgSwitch', descripcion: 'Texto de la sección 3' }
+  ];
+ 
+indiceActual = 0;
+  
+anterior() {
+  this.indiceActual =
+    (this.indiceActual - 1 + this.seccion.length) % this.seccion.length;
+}
+ 
+siguiente() {
+  this.indiceActual =
+    (this.indiceActual + 1) % this.seccion.length;
+}
 }
--- qualification/src/app/header/header.ts
+++ qualification/src/app/header/header.ts
@@ -7,23 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './header.css'
 })
 export class Header {
-  seccion= [
-    "En Angular, las directivas son clases que permiten agregar comportamientos adicionales a elementos del DOM o incluso transformar estos elementos y su contenido. Piensa en las directivas como instrucciones que puedes aplicar a los elementos de tu página para manipular sus propiedades, estructura, o incluso su comportamiento en tiempo de ejecución.",
-    { titulo: 'Info 1', descripcion: 'Texto de la sección 1' },
-    { titulo: 'Info 2', descripcion: 'Texto de la sección 2' },
-    { titulo: 'Info 3', descripcion: 'Texto de la sección 3' }
-];
- 
-indiceActual = 0;
-  
-anterior() {
-  this.indiceActual =
-    (this.indiceActual - 1 + this.seccion.length) % this.seccion.length;
-}
- 
-siguiente() {
-  this.indiceActual =
-    (this.indiceActual + 1) % this.seccion.length;
-}
+
 
 }
